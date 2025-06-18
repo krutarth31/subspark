7e89fd67a2a5f386cb214409febdce85cf7c62c5
krutarth31 <66749184+krutarth31@users.noreply.github.com>
2025-06-18 19:24:06 -0400

    Add theme toggle

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import { SidebarTrigger } from "@/components/ui/sidebar"
+import { ModeToggle } from "@/components/mode-toggle"
 
 export function SiteHeader({ title }: { title: string }) {
   return (
@@ -13,6 +14,7 @@ export function SiteHeader({ title }: { title: string }) {
         />
         <h1 className="text-base font-medium">{title}</h1>
         <div className="ml-auto flex items-center gap-2">
+          <ModeToggle />
           <Button variant="ghost" asChild size="sm" className="hidden sm:flex">
             <a
               href="https://github.com/shadcn-ui/ui/tree/main/apps/v4/app/(examples)/dashboard"
